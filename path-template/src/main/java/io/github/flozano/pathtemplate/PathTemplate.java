ec34a10cadfaa736a4c822903f82ac611f138466
Francisco A. Lozano <flozano@gmail.com>
2024-03-30 15:11:14 +0100

    Initial commit

---
+++ path-template/src/main/java/io/github/flozano/pathtemplate/PathTemplate.java
@@ -0,0 +1,123 @@
+package io.github.flozano.pathtemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.google.api.pathtemplate.ValidationException;
+
+public final class PathTemplate {
+	private final com.google.api.pathtemplate.PathTemplate engine;
+	private final String prefix;
+	private final Map<String, List<String>> varsWhichNeedRepeatedProcessing;
+
+	public PathTemplate(String template) {
+		if (template.startsWith("//")) {
+			throw new IllegalArgumentException("Template cannot start with '//':" + template);
+		}
+		if (template.endsWith("//")) {
+			throw new IllegalArgumentException("Template cannot end in '//': " + template);
+		}
+		if (template.endsWith("/")) {
+			throw new IllegalArgumentException("Template cannot end with '/': " + template);
+		}
+
+		if (template.startsWith("/")) {
+			this.prefix = "/";
+		} else {
+			this.prefix = "";
+		}
+		engine = com.google.api.pathtemplate.PathTemplate.createWithoutUrlEncoding(template);
+		// ensure no var contains two modifiers
+		engine.vars().stream() //
+				.filter(v -> occurrencesOfCharGreaterThan(v, '?', 1)) //
+				.findAny() //
+				.ifPresent(v -> {
+					throw new IllegalArgumentException("Variable cannot contain two modifiers: " + v);
+				});
+		// Check for repeated variables
+		this.varsWhichNeedRepeatedProcessing = engine.vars().stream() //
+				.filter(v -> v.contains("#"))    //
+				.map(v -> v.contains("?") ? v.split("\\?")[0] : v) //
+				.collect(Collectors.groupingBy(v -> v.split("#")[0], Collectors.toList()));
+	}
+
+	public static String render(String template, Map<String, String> variables) {
+		return new PathTemplate(template).render(variables);
+	}
+
+	public String render(Map<String, String> variables) {
+		Map<String, String> processed = new HashMap<>(Objects.requireNonNull(variables));
+		// Process the values and the repeated
+		for (String key : variables.keySet()) {
+			// replace null with ""
+			if (variables.get(key) == null) {
+				processed.replace(key, "");
+			} else if (variables.get(key).isEmpty()) {
+				processed.replace(key, "");
+			}
+			if (varsWhichNeedRepeatedProcessing.containsKey(key)) {
+				for (String repeatedValue : varsWhichNeedRepeatedProcessing.get(key)) {
+					processed.put(repeatedValue, processed.get(key));
+				}
+			}
+		}
+		System.out.println("processed: " + processed);
+		// Process the modifiers
+		Map<String, String> modifiedValues = new HashMap<>(processed);
+		for (String key : processed.keySet()) {
+			PROCESSORS.forEach((suffix, processor) -> {
+				modifiedValues.put(key + "?" + suffix, processor.apply(processed.get(key)));
+			});
+		}
+		System.out.println("modified: " + modifiedValues);
+
+		try {
+			return prefix + engine.instantiate(modifiedValues);
+		} catch (ValidationException e) {
+			throw new IllegalArgumentException("Invalid bindings", e);
+		}
+	}
+
+	Map<String, ValueProcessor> PROCESSORS = Map.of( //
+			"uc", new ValueProcessor(0, String::toUpperCase), //
+			"lc", new ValueProcessor(0, String::toLowerCase), //
+			"ucfirst", new ValueProcessor(0, s -> s.substring(0, 1).toUpperCase() + s.substring(1)), //
+			"lcfirst", new ValueProcessor(0, s -> s.substring(0, 1).toLowerCase() + s.substring(1)) //
+	);
+
+	private static boolean occurrencesOfCharGreaterThan(String s, char c, int n) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == c) {
+				count++;
+			}
+			if (count > n) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
+
+final class ValueProcessor {
+
+	private final int minimumLength;
+	private final Function<String, String> processor;
+
+	ValueProcessor(int minimumLength, Function<String, String> processor) {
+		this.minimumLength = minimumLength;
+		this.processor = processor;
+	}
+
+	public String apply(String s) {
+		if (s == null || s.isEmpty() || s.length() < minimumLength) {
+			return s;
+		}
+		return processor.apply(s);
+	}
+}
---
+++ path-template/src/test/java/io/github/flozano/pathtemplate/PathTemplateTest.java
@@ -0,0 +1,143 @@
+package io.github.flozano.pathtemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class PathTemplateTest {
+
+	@Test
+	public void testSimpleNPE() {
+		try {
+			PathTemplate.render("a/{b}", null);
+			fail();
+		} catch (NullPointerException e) {
+			// OK
+		}
+	}
+
+	@Test
+	public void testEmptyMapVariables() {
+		try {
+			PathTemplate.render("a/{b}", Map.of());
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+	}
+
+	@Test
+	public void testMissingVariableInMap() {
+		try {
+			PathTemplate.render("a/{b}/{c}", Map.of("b", "value"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+	}
+
+	@Test
+	public void testExtraVariablesInMap() {
+		assertEquals("a/value", PathTemplate.render("a/{b}", Map.of("b", "value", "c", "unused")));
+	}
+
+	@Test
+	public void testRepeatedVariables() {
+		assertEquals("value/value/VALUE", PathTemplate.render("{b}/{b#another}/{b#third?uc}", Map.of("b", "value")));
+	}
+
+	@Test
+	public void testBracesInVariableValues() {
+		assertEquals("a/{value}", PathTemplate.render("a/{b}", Map.of("b", "{value}")));
+	}
+
+	@Test
+	public void testMapWithNullValues() {
+		Map<String, String> values = new HashMap<>();
+		values.put("b", null);
+		assertEquals("a/", PathTemplate.render("a/{b}", values));
+	}
+
+	@Test
+	public void testSimpleVariablesWithMultipleAppearances() {
+		assertEquals("whatever/123/123",
+				PathTemplate.render("whatever/{xxx#firstappearance}/{xxx#second}", Map.of("xxx", "123")));
+	}
+
+	@Test
+	public void testSimpleVariables() {
+		assertEquals("whatever/123/456",
+				PathTemplate.render("whatever/{xxx}/{yyy}", Map.of("xxx", "123", "yyy", "456")));
+
+		assertEquals("/whatever/123/456",
+				PathTemplate.render("/whatever/{xxx}/{yyy}", Map.of("xxx", "123", "yyy", "456")));
+	}
+
+	@Test
+	public void testVariablesWithLc() {
+		assertEquals("whatever/name/surname",
+				PathTemplate.render("whatever/{xxx?lc}/{yyy?lc}", Map.of("xxx", "NAME", "yyy", "SURNAME")));
+	}
+
+	@Test
+	public void testVariablesWithUc() {
+		assertEquals("whatever/NAME/SURNAME",
+				PathTemplate.render("whatever/{xxx?uc}/{yyy?uc}", Map.of("xxx", "name", "yyy", "surname")));
+	}
+
+	@Test
+	public void testVariablesWithUcfirst() {
+		assertEquals("whatever/Name/Surname/S",
+				PathTemplate.render("whatever/{xxx?ucfirst}/{yyy?ucfirst}/{zzz?ucfirst}",
+						Map.of("xxx", "name", "yyy", "surname", "zzz", "s")));
+	}
+
+	@Test
+	public void testVariablesWithLcfirst() {
+		assertEquals("whatever/nAME/sURNAME",
+				PathTemplate.render("whatever/{xxx?lcfirst}/{yyy?lcfirst}", Map.of("xxx", "NAME", "yyy", "SURNAME")));
+	}
+
+	@Test
+	public void testBadTemplates() {
+		try {
+			PathTemplate.render("a/{b}/", Map.of("b", "123"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+
+		try {
+			PathTemplate.render("/a/{b}/", Map.of("b", "123"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+
+		try {
+			PathTemplate.render("//a/{b}/", Map.of("b", "123"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+
+		try {
+			PathTemplate.render("/a/{b}//", Map.of("b", "123"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+
+		try {
+			PathTemplate.render("/a/{b?uc?lc}", Map.of("b", "123"));
+			fail();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+	}
+
+}
